8d7209dc1bf59efb206cf6f8776206e37a669845
Katy <115113751+ShidliKaty@users.noreply.github.com>
2023-11-16 19:49:03 +0300

    add loading issues to tests

--- client/src/pages/PostList.test.jsx
+++ client/src/pages/PostList.test.jsx
@@ -1,13 +1,13 @@
-import { describe, it, expect } from "vitest"
-import { screen } from "@testing-library/react"
-import { renderRoute } from "../../test-setup/renderRoute"
-import { addMockApiRouteHandler } from "../../test-setup/mockServer"
-import { HttpResponse } from "msw"
-import userEvent from "@testing-library/user-event"
+import { describe, it, expect } from "vitest";
+import { screen, waitFor } from "@testing-library/react";
+import { renderRoute } from "../../test-setup/renderRoute";
+import { addMockApiRouteHandler } from "../../test-setup/mockServer";
+import { HttpResponse } from "msw";
+import userEvent from "@testing-library/user-event";
 
 describe("PostList page", () => {
   it("properly filters the post list based on filter inputs", async () => {
-    const user = userEvent.setup()
+    const user = userEvent.setup();
     addMockApiRouteHandler("get", "/posts", ({ request }) => {
       const posts = [
         {
@@ -22,19 +22,19 @@ describe("PostList page", () => {
           body: "second post body",
           userId: 2,
         },
-      ]
+      ];
       return HttpResponse.json(
-        posts.filter(post => {
-          const searchParams = new URL(request.url).searchParams
-          const title = searchParams.get("q") || ""
-          const userId = parseInt(searchParams.get("userId"))
+        posts.filter((post) => {
+          const searchParams = new URL(request.url).searchParams;
+          const title = searchParams.get("q") || "";
+          const userId = parseInt(searchParams.get("userId"));
           return (
             post.title.includes(title) &&
             (isNaN(userId) || post.userId === userId)
-          )
+          );
         })
-      )
-    })
+      );
+    });
 
     addMockApiRouteHandler("get", "/users", () => {
       return HttpResponse.json([
@@ -46,30 +46,36 @@ describe("PostList page", () => {
           id: 2,
           name: "second user",
         },
-      ])
-    })
+      ]);
+    });
 
-    renderRoute("/posts")
+    renderRoute("/posts");
 
-    expect(await screen.findByText("first post")).toBeInTheDocument()
-    expect(screen.getByText("second post")).toBeInTheDocument()
+    expect(await screen.findByText("first post")).toBeInTheDocument();
+    expect(screen.getByText("second post")).toBeInTheDocument();
 
-    const queryInput = screen.getByLabelText("Query")
-    const filterBtn = screen.getByText("Filter")
-    await user.type(queryInput, "first")
-    await user.click(filterBtn)
+    const queryInput = screen.getByLabelText("Query");
+    const filterBtn = screen.getByText("Filter");
+    await user.type(queryInput, "first");
+    await user.click(filterBtn);
 
-    expect(screen.getByText("first post")).toBeInTheDocument()
-    expect(screen.queryByText("second post")).not.toBeInTheDocument()
-    expect(queryInput).toHaveValue("first")
+    await waitFor(() =>
+      expect(screen.queryByText("Loading...")).not.toBeInTheDocument()
+    );
+    expect(screen.getByText("first post")).toBeInTheDocument();
+    expect(screen.queryByText("second post")).not.toBeInTheDocument();
+    expect(queryInput).toHaveValue("first");
 
-    const userInput = screen.getByLabelText("Author")
-    await user.selectOptions(userInput, "second user")
-    await user.clear(queryInput)
-    await user.click(filterBtn)
+    const userInput = screen.getByLabelText("Author");
+    await user.selectOptions(userInput, "second user");
+    await user.clear(queryInput);
+    await user.click(filterBtn);
 
-    expect(screen.queryByText("first post")).not.toBeInTheDocument()
-    expect(screen.getByText("second post")).toBeInTheDocument()
-    expect(userInput).toHaveValue("2")
-  })
-})
+    await waitFor(() =>
+      expect(screen.queryByText("Loading...")).not.toBeInTheDocument()
+    );
+    expect(screen.queryByText("first post")).not.toBeInTheDocument();
+    expect(screen.getByText("second post")).toBeInTheDocument();
+    expect(userInput).toHaveValue("2");
+  });
+});
